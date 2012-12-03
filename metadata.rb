8ca7035da1ee0d7cf8e788cbd06d2811be01b2a0
alex <alex.valiushko@cybera.ca>
2012-12-03 16:20:48 -0700

    added  attribute

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "alex.valiushko@cybera.ca"
 license          "Apache 2.0"
 description      "Installs and configures Apache VCL"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 %w{ centos rhel }.each do |os|
   supports os
